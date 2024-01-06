f591be8553486ea312e714692ba64209960a325a
vicktoh <ayomidevictor349@gmail.com>
2024-01-06 11:21:38 +0100

    Changed the location of the margin

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,16 @@
 import React, { Children } from 'react'
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 const Layout = ({ children }) =>
 {
     return (
         <div>
-            <Navbar />
-            {children}
+            <div className='mt-[30px]  mx-[150px] '>
+                <Navbar />
+                {children}
+            </div>
+            <Footer />
         </div>
     )
 }
